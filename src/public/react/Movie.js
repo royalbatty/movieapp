a1a72262dd30533cb7e92731e294714f68957d47
Friendo Polythemus <whowasitpolythemus@protonmail.com>
2020-08-10 15:57:53 -0700

    Loading movie data on render

--- src/public/react/Movie.js
+++ src/public/react/Movie.js
@@ -1,9 +1,25 @@
-import React from "react"; 
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { apikey } from '../../../config.js';
 
-const Movie = ({movieData}) => (
-  <div>
-    <h1>Movie Page!</h1>
-  </div>
-);
+const Movie = () => {
+
+  const { id } = useParams();
+  const [movie, setMovie] = useState({});
+ 
+  useEffect(()=> {
+    fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}&language=en-US`)
+    .then( res => res.json())
+    .then( movie => {console.log(movie); setMovie(movie)})
+    },
+    []
+  )
+
+  return (
+    <div>
+      <h1>{movie.title}</h1>
+    </div>
+  )
+}
 
 export default Movie;

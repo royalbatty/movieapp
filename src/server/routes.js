71f0ad1b1c73bf4a62ff5e69cf3145cafa749807
Friendo Polythemus <whowasitpolythemus@protonmail.com>
2020-08-10 17:39:29 -0700

    Route to get popular movies is backendified

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const axios = require('axios');
+const { apikey } = require( '../../config.js');
 
 app.use(express.static(path.join(__dirname, '..', '..', 'dist')));
 app.use('/css/', express.static(path.join(__dirname, '..', 'public', 'css')));
@@ -8,6 +10,19 @@ app.use('/css/', express.static(path.join(__dirname, '..', 'public', 'css')));
 app.use((req, res, next) => {
   console.log(`Incoming ${req.method} request to ${req.path}`);
   next();
+});
+
+app.get('/api/movies', (req, res) => {
+  axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`)
+  .then((result) => {
+     if( result === null ) {
+        res.status( 404 ).send( 'No movie list found' );
+     }
+    res.status(200).send( result.data );
+  })
+  .catch( function( error ) {
+    return error;
+  });
 });
 
 app.get('*', (req, res) => {

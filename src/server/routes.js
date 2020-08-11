c966e5ea974305037bb3ea18b42747a975b13822
Friendo Polythemus <whowasitpolythemus@protonmail.com>
2020-08-10 18:13:27 -0700

    Search route on server side

--- src/server/routes.js
+++ src/server/routes.js
@@ -16,17 +16,45 @@ app.get('/api/movies', (req, res) => {
   axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${apikey}&language=en-US&page=1`)
   .then((result) => {
      if( result === null ) {
-        res.status( 404 ).send( 'No movie list found' );
+        res.status(404).send('No movie list found');
      }
     res.status(200).send( result.data );
   })
   .catch( function( error ) {
-    return error;
+    res.status(500).send('Error making a request');
+  });
+});
+
+app.get('/api/movies/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`https://api.themoviedb.org/3/movie/${id}?api_key=${apikey}&language=en-US`)
+  .then((result) => {
+     if( result === null ) {
+        res.status( 404 ).send('No such movie');
+     }
+    res.status(200).send(result.data);
+  })
+  .catch(function( error) {
+    res.status(500).send('Error making a request');
   });
 });
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', '..', 'index.html'));
 });
+
+app.get('/api/search/movies/:query', (req, res) => {
+  const query = req.params.query;
+  axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${apikey}&language=en-US&page=1&query=${query}`)
+   .then((result) => {
+     if( result === null ) {
+        res.status( 404 ).send('No such movie');
+     }
+    res.status(200).send(result.data);
+  })
+  .catch(function( error) {
+    res.status(500).send('Error making a request');
+  });
+})
 
 module.exports = app;
